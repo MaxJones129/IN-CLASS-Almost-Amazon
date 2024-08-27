d907eafdd5afab91a02304db6ba6321d37132a83
MaxJones129 <maxjones129@gmail.com>
2024-08-26 20:18:53 -0500

    Made all of CRUD work, now adding uid events

---
+++ pages/authorBooks.js
@@ -0,0 +1,20 @@
+import clearDom from '../utils/clearDom';
+import renderToDOM from '../utils/renderToDom';
+
+const showAuthorBooks = (array) => {
+  clearDom();
+
+  const btnString = '<button class="btn btn-success btn-lg mb-4" id="add-book-btn">Add A Book</button>';
+  renderToDOM('#add-button', btnString);
+
+  let domString = '';
+  array.forEach((item) => {
+    domString += `
+      <div class="card">
+        <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
+      </div>`;
+  });
+  renderToDOM('#authorBooks', domString);
+};
+
+export default showAuthorBooks;
